47cbeed397fdfd831e7164e912fcab952ab22005
Marek Pszczolka <feorin94@gmail.com>
2026-06-01 18:50:37 +0200

    changes13-r1-v3

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,7 +1,8 @@
-from .tools import UnifiedMCPGateway
+from .tools import UnifiedMCPGateway, repl_tool
 from .vector_tools import search_zoning_laws
 
 __all__ = [
     "UnifiedMCPGateway",
+    "repl_tool",
     "search_zoning_laws",
 ]
